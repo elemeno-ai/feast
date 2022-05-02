ae719133515299f4f2cd353fff12fb22faacf585
bruno <oliveira.ufrj11@gmail.com>
2022-05-02 13:50:35 -0300

    Add new scopes to appflow authentication

--- sdk/python/feast/infra/utils/gcp_utils.py
+++ sdk/python/feast/infra/utils/gcp_utils.py
@@ -5,8 +5,13 @@ from typing import List
 from google.auth.credentials import Credentials
 from google_auth_oauthlib import flow
 
+FILE_PATH = "credentials/client_secret.json"
+SCOPES = [
+    "https://www.googleapis.com/auth/bigquery", 
+    "https://www.googleapis.com/auth/devstorage.read_write"
+]
 
-def get_appflow(file_path: str = "client_secret.json", scopes: List[str] = ["https://www.googleapis.com/auth/bigquery"]) -> Credentials:
+def get_appflow(file_path: str = FILE_PATH, scopes: List[str] = SCOPES) -> Credentials:
     """
     Get the Google AppFlow for BigQuery offline store.
     This method is a no-op if ELEMENO_MODE is not production, in this case authentication happens through service account.
